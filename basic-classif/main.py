12bc54253b031b2e87eac3eff1f5034d3f22b91e
Jakub Flaska <jakub.flaska@nih.gov>
2018-09-11 16:18:34 -0400

    x

---
+++ basic-classif/main.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+import get_data
+(train_images, train_labels), (test_images, test_labels) = get_data.load_data()
+
+
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
